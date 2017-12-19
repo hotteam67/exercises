dfa9c56ab56ee6315f4ff6cc099d946e8cf8f84d
mas2 <mas2@DESKTOP-D9A0SDE>
2017-12-19 15:53:25 -0500

    Exercise 03 completed

--- src/benchTest.cpp
+++ src/benchTest.cpp
@@ -2,72 +2,69 @@
 #include "RobotUtils/RobotUtils.h"
 #include "ctrlib/CANTalon.h"
 #include "ctrlib/PigeonImu.h"
+#include <chrono>
 
-/* Exercise 01 is to use the joystick and motors (Talon SRX) 2, 3, 4 on the Sweet Bench
+/* Exercise 03 uses the Potentiometer (AnalogIn0), Pushbutton Switch (DIO4),
+ * light sensor (DIO5) and motor 1 (CANTalon1) on the Sweet Bench.
+ * It is also necessary to use the <chrono> library to precisely measure the
+ * time period of the pulses detected from the light sensor.
+ *
+ * It should be noted that the wheel on top of motor 1 (which is the target of
+ * the light sensor) has two openings.  Therefore, the light sensor should see
+ * a transition from FALSE to TRUE two times per revolution.
  *
  * Functional Requirements:
  * 1)  All functionality shall be done in TeleOp mode.
- * 2)  Use the joystick "A" button to cycle between controlling motor 2, motor 3, motor 4, or no motor.
- * 3)  Upon initialization, the control shall default to "no motor" for safety.
- * 4)  Button presses shall be interpreted as the transition from false to true.
- * 5)  The joystick left stick, x-axis shall control the speed and direction of the active motor.
- * 6)  The x-axis of the left stick should have a deadband from -0.2 to +0.2 to allow for
- *     joystick hysteresis (i.e. not quite returning to zero when released)
- * 7)  The lower "active" region of the joystick (-1.0 -> -0.2) shall command between -1.0 and 0.0 speed.
- * 8)  The upper "active" region of the joystick (+0.2 -> +1.0) shall command between 0.0 and +1.0 speed.
- * 9)  The A Button State, Commanded Speed, and Active Motor shall be output to the dashboard.
+ * 2)  Use the pushbutton switch (DIO4) to select the state of the motor.  This state indicates
+ *     OFF, FORWARD or REVERSE.
+ * 3)  The motor state shall start in the safe state, OFF.
+ * 4)  The potentiometer (AnalogIn0) shall control the speed of motor 1.
+ * 5)  The motor speed command shall range from 0 (off) to 1 (100%) never crossing either limit.
+ * 6)  The light sensor shall be used to calculate the time between false->true transitions observed.
+ * 7)  The time between transitions shall be used to estimate the motor speed in rev/min.  NOTE: This
+ *     calculation can be easily corrupted if the motor is spinning fast.  This is because loop times
+ *     for the RoboRIO are around 20 ms.  In can be reasoned that the fastest that can be "accurately"
+ *     measured is if the light sensor changes state every loop - meaning that all 4 states of the
+ *     target wheel are observed each rotation.  This means that rotations that finish in less
+ *     than (20 ms * 4), or 80 ms may be corrupted.  1 rotation every 80ms equates to 750 rpm.
+ * 8)  Output time between transitions, estimated speed (rpm), motor state and commanded speed to the
+ *     dashboard.
  *
  */
 
 class benchTest: public HotBot {
 private:
 
-	HotJoystick* m_driver;
-
 	CANTalon* m_CANmotor1;
-	CANTalon* m_CANmotor2;
-	CANTalon* m_CANmotor3;
-	CANTalon* m_CANmotor4;
-	PigeonImu * m_pigey;
 	AnalogInput* m_Analog0;
-	Relay* m_LED_Ring;
-	DigitalInput *m_SwitchDIO4;
+	DigitalInput* m_SwitchDIO4;
+	DigitalInput* m_LightSensor;
 
-	bool aButton;
-	bool aButtonOld = false;
-	bool motorStTransInProcess = false;
-	bool ledTransInProcess = false;
 	bool switchSignal;
 	bool switchSignalOld = false;
-
-	float joystickRaw;
-	float joystickMod;
-	float spdCmd;
-	double angle[3];
+	bool lightSignal;
+	bool lightSignalOld;
 	float pot1;
-	float pot2;
+	float spdCmd;
+	std::chrono::time_point<std::chrono::high_resolution_clock> timeNow = std::chrono::high_resolution_clock::now();
+	std::chrono::time_point<std::chrono::high_resolution_clock> timeLast = std::chrono::high_resolution_clock::now();
+	double timeDelta = 0.0; /* milliseconds */
+	double speedRPM = 0.0; /* motor speed calculation in rpm */
 
-	bool ledSelect = false;
-	     /* false: LED off
-	        true:  LED on */
+	int motorState = 0;
+	      /* motorState = 0 : motor OFF
+	       * motorState = 1 : FORWARD
+	       * motorState = 2 : REVERSE
+	       */
 
-	int motorSelect = 0;
-	     /* 0: Off (no motor)
-		    1: CANTalon2
-			2: CANTalon3
-			3: CANTalon4 */
 
 public:
 	benchTest() {
-		m_driver = new HotJoystick(0);
 		m_CANmotor1 = new CANTalon(1);
-		m_CANmotor2 = new CANTalon(2);
-		m_CANmotor3 = new CANTalon(3);
-		m_CANmotor4 = new CANTalon(4);
-		m_pigey = new PigeonImu(m_CANmotor4);  /* Pigeon installed on CANTalon(4) */
 		m_Analog0 = new AnalogInput(0);  /* Analog Input Channel 0 */
-		m_LED_Ring = new Relay(1);
 		m_SwitchDIO4 = new DigitalInput(4);
+		m_LightSensor = new DigitalInput(5);
+		lightSignalOld = m_LightSensor->Get();
 
 	}
 	void RobotInit() {
@@ -87,121 +84,85 @@ public:
 	}
 
 	void TeleopPeriodic() {
-
-		 /* Read inputs from Joystick */
-		 aButton = m_driver->ButtonA();
-		 joystickRaw = m_driver->AxisLX();  /* Uses Left Stick, X axis */
-
 		 /* Read input from DIO4 - pushbutton switch */
 		 switchSignal = !m_SwitchDIO4->Get();
 
-		 /* Read potentiometer inputs from Analog0 and CANmotor2 */
+		 /* Read input from DIO5 - light sensor */
+		 lightSignal = m_LightSensor->Get();;
+
+		 /* Read potentiometer inputs from Analog0 */
 		 pot1 = m_Analog0->GetValue();
-		 pot2 = m_CANmotor2->GetAnalogIn();
-
-		 /* Write Relay for LED ring */
-		 m_LED_Ring->Set(Relay::Value::kOff);
-
-		 /* Deadband input, determine joystickMod */
-		 if(joystickRaw <= 0.2 && joystickRaw >= -0.2){
-			 joystickMod = 0.0;
-		 }else if(joystickRaw > 0.2){
-			 joystickMod = (joystickRaw - 0.2) * 1.25;
-		 }else{
-			 joystickMod = (joystickRaw + 0.2) * 1.25;
-		 }
-		 spdCmd = joystickMod;
-
-		 /* Process button presses */
-		 /* Look for aButton to transition from false to true */
-		 if ((aButton == true) && (aButtonOld == false)) {
-			 motorStTransInProcess = true;
-		 } else {
-			 motorStTransInProcess = false;
+		 spdCmd = (pot1 - 15.0) / 3900.0;
+		 if (spdCmd < 0.0) {
+			 spdCmd = 0.0;
+		 } else if (spdCmd > 1.0) {
+			 spdCmd = 1.0;
 		 }
 
-		 /* Look for bButton to transition from false to true */
+		 /* Read clock for loop time. */
+		 timeNow = std::chrono::high_resolution_clock::now();
+		 timeDelta = std::chrono::duration_cast<std::chrono::milliseconds>(timeNow-timeLast).count();
+
+
+		 /* Look for switchSignal (DIO4) to transition from false to true */
 		 if ((switchSignal == true) && (switchSignalOld == false)) {
-			  ledTransInProcess = true;
-		 } else {
-		 	  ledTransInProcess = false;
+			 if (motorState == 2) {
+				 motorState = 0;
+			 } else {
+				 motorState++;
+			 }
+
 		 }
 
-		 /* Use motorSelect to define speed commands to motors */
-		 if (motorStTransInProcess == true) {
-			 if (motorSelect < 3) {
-				 motorSelect++;
+		 /* Look for lightSignal (DIO5) to transition from false to true */
+		 /* Calculate speed based on knowledge that the variable timeDelta will measure
+		  * the number of milliseconds it takes for the motor to spin 1/2 revolution.
+		  * Therefore, the speed in revs/min = 30,000/timeDelta.  Note - this calculation can
+		  * really be corrupted if motor speeds are fast.  Knowing the loop rate of the
+		  * RoboRIO is 20 ms, the fasted possible speed to detect is 750 rpm.  Anything over
+		  * that will be corrupted.
+		  */
+		 if ((lightSignal == true) && (lightSignalOld == false)) {
+			 timeNow = std::chrono::high_resolution_clock::now();
+			 timeDelta = std::chrono::duration_cast<std::chrono::milliseconds>(timeNow-timeLast).count();
+			 timeLast = timeNow;
+			 if (timeDelta > 0.0) {
+				 speedRPM = (30000.0 / timeDelta);
 			 } else {
-				 motorSelect = 0;
+				 speedRPM = 0.0;
 			 }
 		 }
 
-		 /* Change LED state when ledTransInProccess == true */
-		 if (ledTransInProcess == true) {
-			 ledSelect = !ledSelect;
-		 }
-
-		 /* Send LED command to relay */
-		 if (ledSelect == true) {
-			 m_LED_Ring->Set(Relay::Value::kForward);
+		 /* Send speed command to motor based on motorState variable */
+		 if (motorState == 0) {
+			 m_CANmotor1->Set(0.0);
+		 } else if (motorState == 1 ){
+			 m_CANmotor1->Set(spdCmd);
 		 } else {
-			 m_LED_Ring->Set(Relay::Value::kOff);
-		 }
-
-		 /* Command speeds to motor controllers */
-		 switch (motorSelect) {
-		 case 0:
-			 m_CANmotor2->Set(0.0);
-			 m_CANmotor3->Set(0.0);
-			 m_CANmotor4->Set(0.0);
-			 break;
-		 case 1:
-			 m_CANmotor2->Set(spdCmd);
-			 m_CANmotor3->Set(0.0);
-			 m_CANmotor4->Set(0.0);
-			 break;
-		 case 2:
-			 m_CANmotor2->Set(0.0);
-			 m_CANmotor3->Set(spdCmd);
-			 m_CANmotor4->Set(0.0);
-			 break;
-		 case 3:
-			 m_CANmotor2->Set(0.0);
-			 m_CANmotor3->Set(0.0);
-			 m_CANmotor4->Set(spdCmd);
-			 break;
+			 m_CANmotor1->Set(-spdCmd);
 		 }
 
 		 /* Preserve knowledge of previous loop button state */
-		 aButtonOld = aButton;
 		 switchSignalOld = switchSignal;
-
-		 /* Read the Pigeon IMU for Yaw, Pitch, Roll */
-		 m_pigey->GetYawPitchRoll(angle);
+		 lightSignalOld = lightSignal;
 
 		 DashboardOutput();
 	}
 
 	void DashboardOutput() {
 		/* Writes variables to Dashboard */
-		SmartDashboard::PutBoolean("ButtonA", aButton);
 		SmartDashboard::PutBoolean("switchSignal", switchSignal);
-		SmartDashboard::PutNumber("joystickRaw", joystickRaw);
-		SmartDashboard::PutNumber("joystickMod", joystickMod);
-		SmartDashboard::PutNumber("motorSelect", motorSelect);
-		SmartDashboard::PutBoolean("ledSelect", ledSelect);
+		SmartDashboard::PutBoolean("lightSignal", lightSignal);
+        SmartDashboard::PutNumber("timeDelta", timeDelta);
+        SmartDashboard::PutNumber("speedRPM", speedRPM);
+		SmartDashboard::PutNumber("motorState", motorState);
 		SmartDashboard::PutNumber("SpdCmd", spdCmd);
-		SmartDashboard::PutNumber("Angle0 (Yaw)", angle[0]);
-		SmartDashboard::PutNumber("Angle1 (Pitch)", angle[1]);
-		SmartDashboard::PutNumber("Angle2 (Roll)", angle[2]);
 		SmartDashboard::PutNumber("Pot1", pot1);
-		SmartDashboard::PutNumber("Pot2", pot2);
 	}
 
 
 	void TestPeriodic() {
 	}
-
 };
 
 START_ROBOT_CLASS(benchTest)
