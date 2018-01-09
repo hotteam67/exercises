babb7daa2d1d1eef0455eccaf990ab77c19aadb2
mas2 <mas2@10.0.0.4>
2018-01-08 20:32:24 -0500

    Marlina left this

--- src/benchTest.cpp
+++ src/benchTest.cpp
@@ -25,23 +25,44 @@ private:
 	HotJoystick* m_driver;
 
 	TalonSRX * m_motor2;
+	TalonSRX * m_motor3;
 	TalonSRX * m_motor4;
 
 	PigeonIMU * m_pigeon;
 
 	double angleIMU[3];
 
+	double kP = 0.01;
+	double kI = 0.00;
+	double kD = 0.00;
+
+
+
 public:
 	benchTest() {
 		m_driver = new HotJoystick(0);
 
 		m_motor2 = new TalonSRX(2);
+		m_motor3 = new TalonSRX(3);
 		m_motor4 = new TalonSRX(4);
 
 		m_pigeon = new PigeonIMU(m_motor4);
 
 	}
 	void RobotInit() {
+
+		//m_motor4->Config_kF(0, 0.1097, 0);
+		m_motor3->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0, -1500.0 * 4096 / 600);
+		m_motor3->SetSensorPhase(true);
+
+		m_motor3->ConfigNominalOutputForward(0, 0);
+		m_motor3->ConfigNominalOutputReverse(0, 0);
+		m_motor3->ConfigPeakOutputForward(12, 0);
+		m_motor3->ConfigPeakOutputReverse(-12, 0);
+
+		m_motor3->Config_kP(0, 0.01, 0);
+		m_motor3->Config_kI(0, 0.0, 0);
+		m_motor3->Config_kD(0, 0.0, 0);
 	}
 
 
@@ -61,11 +82,15 @@ public:
 		DashboardOutput();
 
 		if (m_driver->ButtonA()){
-			m_motor4->Set(ControlMode::PercentOutput, 0.25);
+			m_motor3->Set(ControlMode::PercentOutput, 0.25);
+		}
+		else if (m_driver->ButtonB()) {
+			m_motor3->Set(ControlMode::Velocity, 10);
 		}
 		else {
-			m_motor4->Set(ControlMode::PercentOutput, 0.0);
+			m_motor3->Set(ControlMode::PercentOutput, 0.0);
 		}
+
 
 		m_pigeon->GetYawPitchRoll(angleIMU);
 	}
@@ -88,8 +113,12 @@ public:
 			SmartDashboard::PutNumber("TriggerL", m_driver->AxisLT());
 			SmartDashboard::PutNumber("TriggerR", m_driver->AxisRT());
 
+		//TalonPID
+			SmartDashboard::PutNumber("TalonPosition", m_motor3->GetSelectedSensorPosition(0));
+			SmartDashboard::PutNumber("TalonVelocity", m_motor3->GetSelectedSensorVelocity(0));
+
 		//Talon Info
-			SmartDashboard::PutNumber("Motor Percent Output", m_motor2->GetMotorOutputPercent());
+			SmartDashboard::PutNumber("Motor Percent Output", m_motor3->GetMotorOutputPercent());
 
 		//PigeonIMU Info
 			SmartDashboard::PutNumber("Yaw", angleIMU[0]);
