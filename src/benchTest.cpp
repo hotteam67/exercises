c4501b11ea525da37a82375c3f27d25ad9e41b97
not marlina <marlinabowring@hotmail.com>
2018-01-27 13:05:23 -0500

    Motors configured to 2018 updated libraries, for 2017 robot.

--- src/benchTest.cpp
+++ src/benchTest.cpp
@@ -22,15 +22,21 @@ class benchTest: public IterativeRobot {
 private:
 
 	HotJoystick* m_driver;
-	TalonSRX* m_CANmotor2;
-	TalonSRX* m_CANmotor3;
-	TalonSRX* m_CANmotor4;
+	TalonSRX* m_leftFront;
+	TalonSRX* m_leftMiddle;
+	TalonSRX* m_leftRear;
+	TalonSRX* m_rightFront;
+	TalonSRX* m_rightMiddle;
+	TalonSRX* m_rightRear;
 
 	bool aButton;
 	bool aButtonOld = false;
 	bool motorStTransInProcess = false;
 	float joystickRaw;
 	float spdCmd;
+	//encoder position
+	double leftPosition;
+	double rightPosition;
 
 	int motorSelect = 0;
 	     /* 0: Off (no motor)
@@ -40,10 +46,38 @@ private:
 
 public:
 	benchTest() {
+		//set it && forget it
 		m_driver = new HotJoystick(0);
-		m_CANmotor2 = new TalonSRX(2);
-		m_CANmotor3 = new TalonSRX(3);
-		m_CANmotor4 = new TalonSRX(4);
+		m_leftFront = new TalonSRX(7);
+		m_leftMiddle = new TalonSRX(5);
+		m_leftRear = new TalonSRX(6);
+		m_rightFront = new TalonSRX(2);
+		m_rightMiddle = new TalonSRX(4);
+		m_rightRear = new TalonSRX(3);
+
+
+		m_leftFront->SetNeutralMode(Coast);
+		m_leftMiddle->SetNeutralMode(Coast);
+		m_leftRear->SetNeutralMode(Coast);
+		m_rightFront->SetNeutralMode(Coast);
+		m_rightMiddle->SetNeutralMode(Coast);
+		m_rightRear->SetNeutralMode(Coast);
+
+		m_leftMiddle->Set(ControlMode::Follower, 7);
+		m_leftRear->Set(ControlMode::Follower, 7);
+		m_rightMiddle->Set(ControlMode::Follower, 2);
+		m_rightRear->Set(ControlMode::Follower, 2);
+
+		m_rightFront->SetInverted(true);
+		m_rightMiddle->SetInverted(true);
+		m_rightRear->SetInverted(true);
+
+		m_leftFront->ConfigSelectedFeedbackSensor(QuadEncoder, 0, 0);
+		m_rightFront->ConfigSelectedFeedbackSensor(QuadEncoder, 0, 0);
+		m_leftFront->SetSelectedSensorPosition(0, 0, 0);
+		m_rightFront->SetSelectedSensorPosition(0, 0, 0);
+		m_leftFront->SetSensorPhase(true);
+		m_rightFront->SetSensorPhase(true);
 	}
 	void RobotInit() {
 	}
@@ -65,15 +99,16 @@ public:
 
 		 /* Read inputs from Joystick */
 		 aButton = m_driver->ButtonA();
-		 joystickRaw = m_driver->AxisLX();  /* Uses Left Stick, X axis */
+		 joystickRaw = m_driver->AxisLY();  /* Uses Left Stick, Y axis */
+		 //joystickRaw1 = m_driver->AxisRY(); //Uses Right Stick, Y axis
 
 		 /* Deadband input, determine joystickMod */
 		 if(joystickRaw <= 0.2 && joystickRaw >= -0.2){
 			 spdCmd = 0.0;
 		 }else if(joystickRaw > 0.2){
-			 spdCmd = (joystickRaw - 0.2) * 1.25;
+			 spdCmd = (joystickRaw - 0.2) * -1.25;
 		 }else{
-			 spdCmd = (joystickRaw + 0.2) * 1.25;
+			 spdCmd = (joystickRaw + 0.2) * -1.25;
 		 }
 
 		 /* Process button presses */
@@ -86,34 +121,70 @@ public:
 
 		 /* Use motorSelect to define speed commands to motors */
 		 if (motorStTransInProcess == true) {
-			 if (motorSelect < 3) {
+			 if (motorSelect < 6) {
 				 motorSelect++;
 			 } else {
 				 motorSelect = 0;
 			 }
 		 }
 
+		 /*encoders*/
+		 leftPosition = m_leftFront->GetSelectedSensorPosition(0);
+		 rightPosition = m_leftFront->GetSelectedSensorPosition(0);
+
 		 /* Command speeds to motor controllers */
 		 switch (motorSelect) {
 		 case 0:
-			 m_CANmotor2->Set(ControlMode::PercentOutput, 0.0);
-			 m_CANmotor3->Set(ControlMode::PercentOutput, 0.0);
-			 m_CANmotor4->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftFront->Set(ControlMode::PercentOutput, spdCmd);
+			 m_rightFront->Set(ControlMode::PercentOutput, spdCmd);
 			 break;
 		 case 1:
-			 m_CANmotor2->Set(ControlMode::PercentOutput, spdCmd);
-			 m_CANmotor3->Set(ControlMode::PercentOutput, 0.0);
-			 m_CANmotor4->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftFront->Set(ControlMode::PercentOutput, spdCmd);
+			 m_leftMiddle->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftRear->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightFront->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightMiddle->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightRear->Set(ControlMode::PercentOutput, 0.0);
 			 break;
 		 case 2:
-			 m_CANmotor2->Set(ControlMode::PercentOutput, 0.0);
-			 m_CANmotor3->Set(ControlMode::PercentOutput, spdCmd);
-			 m_CANmotor4->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftFront->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftMiddle->Set(ControlMode::PercentOutput, spdCmd);
+			 m_leftRear->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightFront->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightMiddle->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightRear->Set(ControlMode::PercentOutput, 0.0);
 			 break;
 		 case 3:
-			 m_CANmotor2->Set(ControlMode::PercentOutput, 0.0);
-			 m_CANmotor3->Set(ControlMode::PercentOutput, 0.0);
-			 m_CANmotor4->Set(ControlMode::PercentOutput, spdCmd);
+			 m_leftFront->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftMiddle->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftRear->Set(ControlMode::PercentOutput, spdCmd);
+			 m_rightFront->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightMiddle->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightRear->Set(ControlMode::PercentOutput, 0.0);
+			 break;
+		 case 4:
+			 m_leftFront->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftMiddle->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftRear->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightFront->Set(ControlMode::PercentOutput, spdCmd);
+			 m_rightMiddle->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightRear->Set(ControlMode::PercentOutput, 0.0);
+			 break;
+		 case 5:
+			 m_leftFront->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftMiddle->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftRear->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightFront->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightMiddle->Set(ControlMode::PercentOutput, spdCmd);
+			 m_rightRear->Set(ControlMode::PercentOutput, 0.0);
+			 break;
+		 case 6:
+			 m_leftFront->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftMiddle->Set(ControlMode::PercentOutput, 0.0);
+			 m_leftRear->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightFront->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightMiddle->Set(ControlMode::PercentOutput, 0.0);
+			 m_rightRear->Set(ControlMode::PercentOutput, spdCmd);
 			 break;
 		 }
 
@@ -129,6 +200,8 @@ public:
 		SmartDashboard::PutNumber("joystickRaw", joystickRaw);
 		SmartDashboard::PutNumber("motorSelect", motorSelect);
 		SmartDashboard::PutNumber("SpdCmd", spdCmd);
+		SmartDashboard::PutNumber("LeftEncoderPosition", leftPosition);
+		SmartDashboard::PutNumber("RightEncoderPosition", rightPosition);
 	}
 
 
